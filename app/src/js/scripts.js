062778042c1a6109b64b97614424fe876e7116c0
brazleonardo <leonardok3b@gmail.com>
2016-08-18 12:14:36 -0300

    Ajuste Finais do test

--- app/src/js/scripts.js
+++ app/src/js/scripts.js
@@ -3,14 +3,17 @@
   var Init = {
 
     $height: ($(window).height() - 230) + "px",        
-        
+    
+    /*Função que inicia os serviços*/    
     construct: function(){
 
       this.minHeightWrap();
       this.search();
+      this.menuMobile();
 
     },  
 
+    /*Função que seta uma altura no wrap*/
     minHeightWrap: function(){
                                     
         $('#wrap').css("min-height", this.$height);        
@@ -24,6 +27,7 @@
                     
     },
 
+    /*Função que cria animação na busca*/
     search: function(){
 
       $('.sch label').click(function(){
@@ -47,7 +51,51 @@
 
     },
 
+    /*Função que cria um meu para resolução menor de 991px*/
+    menuMobile: function(){
+
+      var menuHTML = '';
+            menuHTML += '<ul>';
+            menuHTML += '<li>'+$('.app-navbar .nv .op .sch').html()+'</li>';
+            menuHTML += $('.app-navbar .nv .menu ul').html();
+            menuHTML += $('.app-navbar .nv .op .dropdown .dropdown-menu').html();
+            menuHTML += '</ul>';
+
+        var menuMobile = false;
+
+        $(window).resize(function(){
+
+          if($(window).width() <= 991){
+            menuMobile = true;
+          }
+
+        });
+
+        if(menuMobile === true || ($(window).width() <= 991)){
+          $('.menu-mobile').html(menuHTML, function(){
+
+          });
+        }
+        else{
+          $('.menu-mobile').html('');
+        }
+
+      $(".btn-menu").click(function () {
+
+        if ($('body').hasClass('open-menu')) {
+          $('body').removeClass("open-menu");
+        }
+        else {
+            $('body').addClass("open-menu");
+        }
+
+      });
+
+    }
+
   };
+
+  
 
   var $init = Init;
   $init.construct();
