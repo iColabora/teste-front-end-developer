ea223133ee6e448cdcda4d648a8440d8459836e0
Julio Luchiari <julio.luchiari@gmail.com>
2016-08-19 00:51:39 -0300

    Formulário de pedidos validado

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,37 @@ $(function () {
     loadContent("#start-process-button", "task.html");
 });
 
+/*var routes = function () {
+  return {
+      home : 'home.html',
+      task : 'task.html',
+      pedidos : {
+          dia : 'orders-per-day.html',
+          solicitante : 'orders-per-requester.html',
+          pendentes : 'pending-orders.html'
+      }
+  };
+};
+
+var loadRoute = function () {
+    var location = window.location.hash.replace('#/', '');
+    if (routes()[location]) {
+        var hue = location.split('/');
+        loadContentByRoute(routes()[location][hue[1]]);
+    }
+    loadContentByRoute(routes()[location]);
+};
+
+var loadContentByRoute = function (html_file) {
+    $.get(html_file, function (data) {
+        $("#content").html(data);
+
+        if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible")) {
+            $('.navbar-collapse').collapse('toggle');
+        }
+    });
+};*/
+
 var loadContent = function (selector, html_file) {
     $(selector).on('click', function (e) {
         $.get(html_file, function (data) {
@@ -22,9 +53,99 @@ function createElement(element, value) {
     return "<" + element + ">" + value + "</" + element + ">";
 }
 
-function createHiddenInput(name, value) {
-    return "<input type='hidden' name='" + name + "' value='" + value + "' />";
+function createHiddenInput(name, value, extra) {
+    return "<input type='hidden' name='" + name + "' value='" + value + "'" + extra + "/>";
 }
+
+var getUfArray = function () {
+    return [
+        "AC",
+        "AL",
+        "AP",
+        "AM",
+        "BA",
+        "CE",
+        "DF",
+        "ES",
+        "GO",
+        "MA",
+        "MT",
+        "MS",
+        "MG",
+        "PA",
+        "PB",
+        "PR",
+        "PE",
+        "PI",
+        "RJ",
+        "RN",
+        "RS",
+        "RO",
+        "RR",
+        "SC",
+        "SP",
+        "SE",
+        "TO"
+    ];
+};
+
+var validateCpf = function (cpf) {
+    cpf = cpf.replace(/[^\d]+/g,'');
+    var numeros, digitos, soma, i, resultado, digitos_iguais;
+    digitos_iguais = 1;
+    if (cpf.length < 11) {
+        return false;
+    }
+    for (i = 0; i < cpf.length - 1; i++) {
+        if (cpf.charAt(i) != cpf.charAt(i + 1)){
+            digitos_iguais = 0;
+            break;
+        }
+    }
+    if (! digitos_iguais) {
+        numeros = cpf.substring(0,9);
+        digitos = cpf.substring(9);
+        soma = 0;
+        for (i = 10; i > 1; i--)
+            soma += numeros.charAt(10 - i) * i;
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0)) {
+            return false;
+        }
+        numeros = cpf.substring(0,10);
+        soma = 0;
+        for (i = 11; i > 1; i--) {
+            soma += numeros.charAt(11 - i) * i;
+        }
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1)) {
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
+};
+
+var validateTelephone = function (tel) {
+  tel = tel.replace(/[^\d]+/g,'');
+  return validateLength(tel, 10, 11);
+};
+
+var validateZipcode = function (zip) {
+  zip = zip.replace(/[^\d]+/g,'');
+  return validateLength(zip, 8, 8);
+};
+
+var validateUf = function (uf) {
+  var ufs = getUfArray();
+  for (var i = 0; i < ufs.length; i++) {
+      if (ufs[i] == uf) {
+          return true
+      }
+  }
+  return false;
+};
 
 var validateLength = function (str, min, max) {
     var length = str.length;
--- src/js/app/task.js
+++ src/js/app/task.js
@@ -2,48 +2,30 @@ $(function () {
   searchZipcode("#cep_entrega", "#endereco_entrega", "#cidade_entrega", "#estado_entrega", "#complemento_entrega", "#complemento_entrega");
   searchZipcode("#cep_solicitante", "#endereco_solicitante", "#cidade_solicitante", "#estado_solicitante", "#complemento_solicitante", "#complemento_solicitante");
   initMasks();
-
-  $("#check_same_address").on('click', function () {
-    var $div = $("#can-disable-by-checkbox");
-    if ($div.hasClass('disabled')) {
-      $div.removeClass('disabled');
-      $("#cep_entrega, #endereco_entrega, #cidade_entrega, #estado_entrega, #complemento_entrega").removeAttr('disabled');
-    } else {
-      $div.addClass('disabled');
-      $("#cep_entrega, #endereco_entrega, #cidade_entrega, #estado_entrega, #complemento_entrega").attr('disabled', 'disabled');
-    }
-  });
-
   initNewInsuranceFormEvent();
-  
-  $("#modal-form-show").on('click', function (e) {
-    e.preventDefault();
-  })
-  
-  $("#task-form").on('submit', function (e) {
-    e.preventDefault();
-
-    $(".required").each(function (index, obj) {
-      var id = $(this).attr('id');
-      if (! $(this).attr('disabled') && $(this).val() == "") {
-        $(this).addClass('has-error');
-        $("#" + id + "_invalido").html('Campo Obrigatório');
-      } else {
-        $(this).removeClass('has-error');
-         $("#" + id + "_invalido").html('');
-      }
-    })
-  });
-
-  $(".required").on('blur', function () {
-    var self = $(this);
-    if (self.val() == "") {
-      self.addClass('has-error');
-    } else {
-      self.removeClass('has-error');
-    }
-  })
+  initTaskFormSubmitEvent();
+  initRequiredValidationFields();
+  initCheckSameAddressCheckbox();
+  initTotal();
 });
+
+var initTotal = function () {
+  $("#can-add-insurance tr").each(function () {
+    var self = $(this);
+    var index = self.data('index');
+    var qtd = $("#insumo_quantidade_" + index).val();
+    var valor = $("#insumo_valor_" + index).val();
+    var result = Number(qtd) * Number(valor);
+    calcTotal(result);
+  });
+};
+
+var calcTotal = function (total) {
+  var $input = $("#order-total-value input");
+  var current = Number($input.val());
+  total += current;
+  $input.val(total);
+};
 
 var searchZipcode = function (field, address, city, district, complement, focus) {
   $(field).on('blur', function () {
@@ -57,8 +39,196 @@ var searchZipcode = function (field, address, city, district, complement, focus)
         $(district).val(data['uf']);
         $(complement).val(data['complemento']);
         $(focus).focus();
-        console.log(data);
       });
+    }
+  });
+};
+
+var validateRequesterInfo = function () {
+  var $nome = $("#nome_solicitante");
+  var $cpf = $("#cpf_solicitante");
+  var $telefone = $("#telefone_solicitante");
+  var $cep = $("#cep_solicitante");
+  var $endereco = $("#endereco_solicitante");
+  var $cidade = $("#cidade_solicitante");
+  var $uf = $("#estado_solicitante");
+
+  var errors = false;
+
+  if (! validateLength($nome.val(), 3, 50)) {
+    $nome.addClass('has-error');
+    $("#nome_solicitante_invalido").html("Preencha o campo com no mínimo 3 caracteres e no máximo 50");
+    errors = true;
+  } else {
+    $nome.removeClass('has-error');
+    $("#nome_solicitante_invalido").html("");
+  }
+
+  if (! validateCpf($cpf.val())) {
+    $cpf.addClass('has-error');
+    $("#cpf_solicitante_invalido").html("CPF inválido");
+    errors = true;
+  } else {
+    $cpf.removeClass('has-error');
+    $("#cpf_solicitante_invalido").html("");
+  }
+
+  if (! validateTelephone($telefone.val())) {
+    $telefone.addClass('has-error');
+    $("#telefone_solicitante_invalido").html("Telefone inválido");
+    errors = true;
+  } else {
+    $telefone.removeClass('has-error');
+    $("#telefone_solicitante_invalido").html("");
+  }
+
+  if (! validateZipcode($cep.val())) {
+    $cep.addClass('has-error');
+    $("#cep_solicitante_invalido").html("CEP inválido");
+    errors = true;
+  } else {
+    $cep.removeClass('has-error');
+    $("#cep_solicitante_invalido").html("");
+  }
+
+  if (! validateLength($endereco.val(), 5)) {
+    $endereco.addClass('has-error');
+    $("#endereco_solicitante_invalido").html("Preencha o campo com no mínimo 5 caracteres");
+    errors = true;
+  } else {
+    $endereco.removeClass('has-error');
+    $("#endereco_solicitante_invalido").html("");
+  }
+
+  if (! validateLength($cidade.val(), 5)) {
+    $cidade.addClass('has-error');
+    $("#cidade_solicitante_invalido").html("Preencha o campo com no mínimo 5 caracteres");
+    errors = true;
+  } else {
+    $cidade.removeClass('has-error');
+    $("#cidade_solicitante_invalido").html("");
+  }
+
+  if (! validateUf($uf.val())) {
+    $uf.addClass('has-error');
+    $("#estado_solicitante_invalido").html("Selecione uma UF válida");
+    errors = true;
+  } else {
+    $uf.removeClass('has-error');
+    $("#estado_solicitante_invalido").html("");
+  }
+
+  return errors;
+};
+
+var validateShippingAddressInfo = function () {
+  var $cep = $("#cep_entrega");
+  var $endereco = $("#endereco_entrega");
+  var $cidade = $("#cidade_entrega");
+  var $uf = $("#estado_entrega");
+  
+  var errors = false;
+  
+  if (! validateZipcode($cep.val())) {
+    $cep.addClass('has-error');
+    $("#cep_entrega_invalido").html("CEP inválido");
+    errors = true;
+  } else {
+    $cep.removeClass('has-error');
+    $("#cep_entrega_invalido").html("");
+  }
+
+  if (! validateLength($endereco.val(), 5)) {
+    $endereco.addClass('has-error');
+    $("#endereco_entrega_invalido").html("Preencha o campo com no mínimo 5 caracteres");
+    errors = true;
+  } else {
+    $endereco.removeClass('has-error');
+    $("#endereco_entrega_invalido").html("");
+  }
+
+  if (! validateLength($cidade.val(), 5)) {
+    $cidade.addClass('has-error');
+    $("#cidade_entrega_invalido").html("Preencha o campo com no mínimo 5 caracteres");
+    errors = true;
+  } else {
+    $cidade.removeClass('has-error');
+    $("#cidade_entrega_invalido").html("");
+  }
+
+  if (! validateUf($uf.val())) {
+    $uf.addClass('has-error');
+    $("#estado_entrega_invalido").html("Selecione uma UF válida");
+    errors = true;
+  } else {
+    $uf.removeClass('has-error');
+    $("#estado_entrega_invalido").html("");
+  }
+
+  return errors;
+};
+
+var validateOrderInfo = function () {
+  return true;
+};
+
+var initCheckSameAddressCheckbox = function () {
+  $("#check_same_address").on('click', function () {
+    var $div = $("#can-disable-by-checkbox");
+    if ($div.hasClass('disabled')) {
+      $div.removeClass('disabled');
+      $("#cep_entrega, #endereco_entrega, #cidade_entrega, #estado_entrega, #complemento_entrega").removeAttr('disabled');
+    } else {
+      $div.addClass('disabled');
+      $("#cep_entrega, #endereco_entrega, #cidade_entrega, #estado_entrega, #complemento_entrega").attr('disabled', 'disabled');
+    }
+  });
+};
+
+var initTaskFormSubmitEvent = function () {
+  $("#task-form").on('submit', function (e) {
+    e.preventDefault();
+
+    $(".required").each(function (index, obj) {
+      var id = $(this).attr('id');
+      if (! $(this).attr('disabled') && $(this).val() == "") {
+        $(this).addClass('has-error');
+        $("#" + id + "_invalido").html('Campo Obrigatório');
+      } else {
+        $(this).removeClass('has-error');
+        $("#" + id + "_invalido").html('');
+      }
+    });
+
+    var errors = true;
+
+    if (! validateRequesterInfo()) {
+      errors = true;
+    }
+    if (! validateShippingAddressInfo()) {
+      errors = true;
+    }
+    if (! validateOrderInfo()) {
+      errors = true;
+    }
+
+    if (! errors) {
+      $('.required').each(function (index, obj){
+        var id = $(this).attr('id');
+        $(this).removeClass('has-error');
+        $("#" + id + "_invalido").html('');
+      });
+    }
+  });
+};
+
+var initRequiredValidationFields = function () {
+  $(".required").on('blur', function () {
+    var self = $(this);
+    if (self.val() == "") {
+      self.addClass('has-error');
+    } else {
+      self.removeClass('has-error');
     }
   });
 };
@@ -109,17 +279,22 @@ var initNewInsuranceFormEvent = function () {
 
       tds += createElement('td', $marca.val() + createHiddenInput('insumo[' + index + '][marca]', $marca.val()));
       tds += createElement('td', $descricao.val() + createHiddenInput('insumo[' + index + '][descricao]', $descricao.val()));
-      tds += createElement('td', $quantidade.val() + createHiddenInput('insumo[' + index + '][quantidade]', $quantidade.val()));
-      tds += createElement('td', $preco.val() + createHiddenInput('insumo[' + index + '][preco]', $preco.val()));
+      tds += createElement('td', $quantidade.val() + createHiddenInput('insumo[' + index + '][quantidade]', $quantidade.val(), "id='insumo_quantidade_" + index + "'"));
+      tds += createElement('td', $preco.val() + createHiddenInput('insumo[' + index + '][preco]', $preco.val(), "id='insumo_preco_'" + index + "'"));
 
       var tr = "<tr data-index='" + index + "'>" + tds + "</tr>";
 
       $("#can-add-insurance").append(tr);
       $("#new-insurance-modal .close").click();
+
+      var total = Number($quantidade.val()) * Number($preco.val());
+      calcTotal(total);
+
       $marca.val("");
       $preco.val("");
       $quantidade.val("");
       $descricao.val("");
+
       return true;
     }
 
